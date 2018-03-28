cc440fa1c2b5b7785c9d5d3b8131aab884d2f034
AIMMY-PC\Aimmy <noppon_aim@hotmail.com>
2018-03-28 21:59:07 +0700

    Preset Online Object.

--- ProtoWorldUnityProject/Assets/SampleScenes/Scripts/NetworkLobbyHook.cs
+++ ProtoWorldUnityProject/Assets/SampleScenes/Scripts/NetworkLobbyHook.cs
@@ -8,11 +8,14 @@ public class NetworkLobbyHook : LobbyHook
     public override void OnLobbyServerSceneLoadedForPlayer(NetworkManager manager, GameObject lobbyPlayer, GameObject gamePlayer)
     {
         LobbyPlayer lobby = lobbyPlayer.GetComponent<LobbyPlayer>();
-        NetworkSpaceship spaceship = gamePlayer.GetComponent<NetworkSpaceship>();
 
+        //NetworkSpaceship spaceship = gamePlayer.GetComponent<NetworkSpaceship>();
+
+        /*
         spaceship.name = lobby.name;
         spaceship.color = lobby.playerColor;
         spaceship.score = 0;
         spaceship.lifeCount = 3;
+        */
     }
 }
